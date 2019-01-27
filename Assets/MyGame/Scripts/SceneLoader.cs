59bd66813f648f057350afb75d4497c54b4a08b5
smeerws <hello@smeerws.at>
2019-01-27 13:46:20 +0100

    added empty Scenes, added scenes to build settings added canvas incl. image color, button, tmp text added prefabed canvas to all scenes made and added (import+sliced) whitebox sprites to assets added and linked SceneLoader script Intro --> Main --> GameOver --> Intro --> ...

---
+++ Assets/MyGame/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    enum Scenes { Intro, Main, GameOver};
+
+    public void LoadIntroScene()
+    {
+        SceneManager.LoadScene((int)Scenes.Intro);
+    }
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene((int)Scenes.Main);
+    }
+
+    public void LoadGameOverScene()
+    {
+        SceneManager.LoadScene((int)Scenes.GameOver);
+    }
+}
